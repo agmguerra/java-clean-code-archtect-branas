d045589c4cecab12512f242670c383ec1b253f9f
Alexandre Guerra <agmguerra@gmail.com>
2021-11-28 18:24:58 -0300

    commit inicial projeto base SpringBoot

---
+++ ccca/src/main/java/br/com/agmg/ccca/CccaApplication.java
@@ -0,0 +1,13 @@
+package br.com.agmg.ccca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CccaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CccaApplication.class, args);
+	}
+
+}
---
+++ ccca/src/test/java/br/com/agmg/ccca/CccaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.agmg.ccca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CccaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
